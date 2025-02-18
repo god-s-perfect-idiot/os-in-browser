9962bdc21d0dde3d21898da73ddc317b18974515
god-s-perfect-idiot <samarmusthafa2014@gmail.com>
2025-02-18 01:02:02 +0000

    Refactor TextField component styling and behavior

--- src/ui-library/TextField.svelte
+++ src/ui-library/TextField.svelte
@@ -1,25 +1,40 @@
 <script>
 	export let text;
-    export let placeholder='Enter text';
-    export let title='Title';
+	export let placeholder = 'Enter text';
+	export let title = 'Title';
+
+	let focussed = false;
 </script>
 
-<div class="w-full h-full textbox">
-    <span class="title">{title}</span>
-	<textarea bind:value={text} class="w-full rounded-md p-2 h-full" placeholder="{placeholder}"></textarea>
+<div class={`textbox h-full w-full ${focussed ? 'focused' : ''}`}>
+	<span class="title relative top-[-0.75rem] left-4 bg-white px-2">{title}</span>
+	<textarea
+		bind:value={text}
+		class="h-full w-full rounded-md p-2"
+		{placeholder}
+		on:focus={() => (focussed = true)}
+        on:blur={() => (focussed = false)}
+	></textarea>
 </div>
 
 <style>
-    .title {
-        color: var(--accent-color);
-    }
+	.title {
+		color: #9c9c9c;
+	}
+	.textbox {
+		/* border-color: var(--accent-color); */
+        border-radius: 0.3rem;
+		border: 2px solid #9c9c9c;
+		resize: none;
+	}
     .textbox textarea {
-        /* border-color: var(--accent-color); */
-        border: 2px solid #cfcfcf;
-        resize: none;
-    }
-    .textbox textarea:focus {
-        border-color: var(--accent-color);
+        border: none;
         outline: none;
+    }
+    .focused {
+        border-color: var(--accent-color) !important;
+    }
+    .focused span {
+        color: var(--accent-color) !important;
     }
 </style>
