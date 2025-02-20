5920ea89c907338df58ece6b5dec4c70b23dc8fc
god-s-perfect-idiot <samarmusthafa2014@gmail.com>
2025-02-20 23:54:14 +0000

    Update Stopwatch UI styling

--- src/apps/Stopwatch.svelte
+++ src/apps/Stopwatch.svelte
@@ -43,26 +43,26 @@
 </script>
 
 <div class="flex h-full w-full flex-col items-center justify-between p-2 gap-4">
-	<div class="mb-4 flex h-[calc(100%-3rem)] items-center justify-center gap-1 rounded-full border-2 border-gray-600 w-full m-6">
+	<div class="mb-4 flex h-[calc(100%-4rem)] items-center justify-center gap-1 rounded-full border-2 border-gray-600 w-full m-6">
 		<span class="text-5xl">{getTime(elapsed)}</span>
 		<span class="text-4xl">{getMS(elapsed)}</span>
 	</div>
-	<div class="flex h-[3rem] w-full justify-center gap-2">
+	<div class="flex h-[4rem] w-full justify-center gap-2">
 		<button
 			on:click={stop}
-			class="flex w-[20%] items-center justify-center rounded bg-secondary px-4 py-2 text-black"
+			class="flex w-[20%] h-full items-center justify-center rounded bg-secondary px-4 py-2 text-black"
 		>
 			<Icon icon="mdi:stop" font-size="1.5rem" />
 		</button>
 		<button
 			on:click={start}
-			class="bg-accent flex w-[60%] items-center justify-center rounded px-4 py-2 text-white"
+			class="bg-accent flex h-full w-[60%] items-center justify-center rounded px-4 py-2 text-white"
 		>
 			<Icon icon="mdi:play" font-size="1.5rem" />
 		</button>
 		<button
 			on:click={reset}
-			class="flex w-[20%] items-center justify-center rounded bg-source px-4 py-2 text-black"
+			class="flex w-[20%] h-full items-center justify-center rounded-full bg-source px-4 py-2 text-black"
 		>
 			<Icon icon="mdi:restart" font-size="1.5rem" />
 		</button>
